167271b66958b3f614f01be90381becd2bd663eb
AIwinski <artur.iwinski@gmail.com>
2020-02-22 19:20:33 +0100

    fix

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -3,7 +3,7 @@ export const API_ROOT = "https://praca-inz-backend.herokuapp.com";
 export const STATIC_FILES_ROOT = API_ROOT + "/uploads/";
 export const PROFILES_PAGE_SIZE = 5;
 export const MESSAGES_PAGE_SIZE = 100;
-export const FALLBACK_AVATAR_URL = "https://icon-library.net/images/default-user-icon/default-user-icon-8.jpg";
+export const FALLBACK_AVATAR_URL = "/default-user.jpg";
 
 
 export const MIN_COMPLETION_RATE = 0;
